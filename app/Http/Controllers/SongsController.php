7576dde26aaa0928e208db594b6085d27963dd4c
Vladimir Morozov <freezer278@gmail.com>
2019-08-07 17:17:27 +0300

    added basic crud for songs

---
+++ app/Http/Controllers/SongsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UpdateSongRequest;
+use App\Http\Resources\SongResource;
+use App\Song;
+use Illuminate\Http\Request;
+
+class SongsController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function index(Request $request)
+    {
+        $songs = Song::query()->get();
+
+        return SongResource::collection($songs);
+    }
+
+    /**
+     * @param Song $song
+     * @return SongResource
+     */
+    public function getSingle(Song $song)
+    {
+        return new SongResource($song);
+    }
+
+    /**
+     * @param UpdateSongRequest $request
+     * @return SongResource
+     */
+    public function create(UpdateSongRequest $request)
+    {
+        $song = Song::create($request->getData());
+
+        return new SongResource($song);
+    }
+
+    /**
+     * @param Song $song
+     * @param Request $request
+     * @return SongResource
+     */
+    public function update(Song $song, UpdateSongRequest $request)
+    {
+        $song->update($request->getData());
+
+        return new SongResource($song);
+    }
+
+    /**
+     * @param Song $song
+     * @return SongResource
+     * @throws \Exception
+     */
+    public function delete(Song $song)
+    {
+        $song->delete();
+
+        return new SongResource($song);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'songs',], function () {
+    Route::get('/', 'SongsController@index');
+    Route::get('/{song}', 'SongsController@getSingle');
+    Route::post('/', 'SongsController@create');
+    Route::put('/{song}', 'SongsController@update');
+    Route::delete('/{song}', 'SongsController@delete');
+});
